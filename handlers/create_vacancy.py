dcd73e35a356626b7fcd52ef012d04de22042e24
artur <tsaltsalkoa@mail.ru>
2023-08-25 10:18:16 +0300

    upgrade 02: add  StateFilters

--- handlers/create_vacancy.py
+++ handlers/create_vacancy.py
@@ -32,56 +32,56 @@ async def sent_employer(callback: CallbackQuery, state: FSMContext):
     await state.set_state(sf.fill_employer)
 
 
-@router.message(sf.fill_employer, F.text)
+@router.message(StateFilter(sf.fill_employer), F.text)
 async def sent_job(message: Message, state: FSMContext):
     await message.answer(texts.fill_job)
     await state.update_data(employer=message.text)
     await state.set_state(sf.fill_job)
 
 
-@router.message(sf.fill_job, F.text)
+@router.message(StateFilter(sf.fill_job), F.text)
 async def sent_salary(message: Message, state: FSMContext):
     await message.answer(texts.fill_salary)
     await state.update_data(job=message.text)
     await state.set_state(sf.fill_salary)
 
 
-@router.message(sf.fill_salary, F.text)
+@router.message(StateFilter(sf.fill_salary), F.text)
 async def sent_minage(message: Message, state: FSMContext):
     await message.answer(texts.fill_minage)
     await state.update_data(salary=message.text)
     await state.set_state(sf.fill_minage)
 
 
-@router.message(sf.fill_minage, F.text)
+@router.message(StateFilter(sf.fill_minage), F.text)
 async def sent_minexp(message: Message, state: FSMContext):
     await message.answer(texts.fill_minexp)
     await state.update_data(minage=message.text)
     await state.set_state(sf.fill_minexp)
 
 
-@router.message(sf.fill_minexp, F.text)
+@router.message(StateFilter(sf.fill_minexp), F.text)
 async def sent_date(message: Message, state: FSMContext):
     await message.answer(texts.fill_date)
     await state.update_data(minexp=message.text)
     await state.set_state(sf.fill_date)
 
 
-@router.message(sf.fill_date, F.text)
+@router.message(StateFilter(sf.fill_date), F.text)
 async def sent_short_dsp(message: Message, state: FSMContext):
     await message.answer(texts.fill_short_dsp)
     await state.update_data(date=message.text)
     await state.set_state(sf.fill_short_dsp)
 
 
-@router.message(sf.fill_short_dsp, F.text)
+@router.message(StateFilter(sf.fill_short_dsp), F.text)
 async def sent_long_dsp(message: Message, state: FSMContext):
     await message.answer(texts.fill_long_dsp)
     await state.update_data(short_dsp=message.text)
     await state.set_state(sf.fill_long_dsp)
 
 
-@router.message(sf.fill_long_dsp, F.text)
+@router.message(StateFilter(sf.fill_long_dsp), F.text)
 async def confirm_vacancy(message: Message, state: FSMContext):
     await message.answer(texts.confirm_vacancy)
     await state.update_data(long_dsp=message.text)

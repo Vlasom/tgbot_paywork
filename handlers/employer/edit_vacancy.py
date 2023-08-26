ad81fc88742308671d6e745f7845eee93c814b5f
artur <tsaltsalkoa@mail.ru>
2023-08-26 10:41:49 +0300

    spam creaye vacanses fix: replace state update

--- handlers/employer/edit_vacancy.py
+++ handlers/employer/edit_vacancy.py
@@ -16,6 +16,18 @@ router = Router()
 router.callback_query.filter(StateFilter(sf.confirm_create))
 
 
+async def sent_preview(message, state):
+    data = await state.get_data()
+
+    await message.answer(texts.confirm_vacancy_txt(data, type_descr="short"), reply_markup=inkb_contact_like_more,
+                         parse_mode="MarkdownV2")
+
+    # сохранение данных и что-то ещё
+    await asyncio.sleep(0.7)
+    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_edit_vac)
+    await state.set_state(sf.confirm_create)
+
+
 @router.callback_query(Text('stop_edit'))
 async def callback_edit_employer(callback: CallbackQuery, state: FSMContext):
     await callback.message.edit_text(texts.confirm_vacancy)
@@ -91,7 +103,7 @@ async def sent_job(message: Message, state: FSMContext):
     await message.answer(texts.edit_employer)
     await state.update_data(employer=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
+    await sent_preview(message, state)
 
 
 @router.message(StateFilter(sf.edit_job), F.text)
@@ -99,7 +111,7 @@ async def sent_salary(message: Message, state: FSMContext):
     await message.answer(texts.edit_job)
     await state.update_data(job=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
+    await sent_preview(message, state)
 
 
 @router.message(StateFilter(sf.edit_salary), F.text)
@@ -107,44 +119,39 @@ async def sent_minage(message: Message, state: FSMContext):
     await message.answer(texts.edit_salary)
     await state.update_data(salary=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
-
+    await sent_preview(message, state)
 
 @router.message(StateFilter(sf.edit_minage), F.text)
 async def sent_minexp(message: Message, state: FSMContext):
     await message.answer(texts.edit_minage)
     await state.update_data(minage=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
-
+    await sent_preview(message, state)
 
 @router.message(StateFilter(sf.edit_minexp), F.text)
 async def sent_date(message: Message, state: FSMContext):
     await message.answer(texts.edit_minexp)
     await state.update_data(minexp=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
-
+    await sent_preview(message, state)
 
 @router.message(StateFilter(sf.edit_date), F.text)
 async def sent_short_dsp(message: Message, state: FSMContext):
     await message.answer(texts.edit_date)
     await state.update_data(date=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
-
+    await sent_preview(message, state)
 
 @router.message(StateFilter(sf.edit_short_dsp), F.text)
 async def sent_long_dsp(message: Message, state: FSMContext):
     await message.answer(texts.edit_short_dsp)
     await state.update_data(short_dsp=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
-
+    await sent_preview(message, state)
 
 @router.message(StateFilter(sf.edit_long_dsp), F.text)
 async def confirm_vacancy(message: Message, state: FSMContext):
     await message.answer(texts.edit_long_dsp)
     await state.update_data(long_dsp=message.text)
     await state.set_state(sf.confirm_create)
-    await message.answer("Выберите, что вы хотите отредактировать", reply_markup=inkb_process_edit_vac)
+    await sent_preview(message, state)

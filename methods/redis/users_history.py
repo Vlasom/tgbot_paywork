a0ca495456b1b2feaf9a1d0322e2e64c6844ac42
presto-manager <Tsaltsalkoa@mail.ru>
2023-09-07 15:37:06 +0900

    add more effective check_history func

--- methods/redis/users_history.py
+++ methods/redis/users_history.py
@@ -1,5 +1,4 @@
 from processes_redis import redis_client
-import timeit
 
 
 def add_history(user_tg_id: int, id: list) -> None:
@@ -7,23 +6,6 @@ def add_history(user_tg_id: int, id: list) -> None:
     redis_client.expire(f"{user_tg_id}_history", 86400)
 
 
-def check_history(user_tg_id: int, id: list): #2.1188024999573827
-    check_res = redis_client.smismember(f"{user_tg_id}_history", *id)
-    unique_id = [id[i] for i in range(len(id)) if check_res[i] == 0]
-    redis_client.expire(f"{user_tg_id}_history", 86400)
-
-    print(unique_id)
-
-
-def check_history2(user_tg_id: int, id: list): #3.7813390000374056
-    redis_client.sadd(f"{user_tg_id}_check", *id)
-    unique_id = redis_client.sdiff(f"{user_tg_id}_check", f"{user_tg_id}_history")
-    redis_client.delete(f"{user_tg_id}_check")
-    redis_client.expire(f"{user_tg_id}_history", 86400)
-
-    print(unique_id)
-
-
-# add_history(12345, [2, 5, 7])
-print(timeit.timeit("check_history2(12345, [1, 2, 3, 4])", globals={"check_history2": check_history2, },
-                    number=1000))
+def check_history(user_tg_id: int, id: list) -> list:  # 1.0432917000143789
+    set_res = set(id) - set(map(int, redis_client.smembers(f"{user_tg_id}_history")))
+    return list(set_res)
